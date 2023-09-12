e4f9898e6822f8dca06936f419104ad546137b0d
Josue <joshvlbn@gmail.com>
2023-09-11 21:52:33 -0300

    rutas products funcionando correctamente, falta persistencia de archivos

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+const prodctsRouter = require('./routes/products');
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', prodctsRouter)
 
 app.listen(port, () => console.log(`Server ON in port ${port}`))
