d0f2b2928bb307a8aa1929571bc3fea1244a2f97
Josue <joshvlbn@gmail.com>
2023-09-12 21:17:26 -0300

    todas las rutas de produtcs funcionando de manera asincronica con FS

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const fs = require('fs').promises;
 const productsRouter = express.Router();
 
 const products = [];
 
-productsRouter.get("/api/products", (req, res) => {
+productsRouter.get("/api/products", async (req, res) => {
     try {
-        res.send(products)
+        const data = await fs.readFile("products.txt", "utf-8");
+        res.send(data)
     } catch (error) {
         res.send({ message: error })
     }
@@ -13,10 +15,11 @@ productsRouter.get("/api/products", (req, res) => {
 
 /* http://localhost:8080/api/products */
 
-productsRouter.get("/api/products/:pid", (req, res) => {
+productsRouter.get("/api/products/:pid", async (req, res) => {
     try {
         const { pid } = req.params;
-        const product = products.find(ele => ele.id === parseInt(pid));
+        const data = await fs.readFile("products.txt", "utf-8");
+        const product = await JSON.parse(data).find(ele => ele.id === parseInt(pid));
         res.send(product)
     } catch (error) {
         res.send({ message: error })
@@ -25,11 +28,9 @@ productsRouter.get("/api/products/:pid", (req, res) => {
 
 /* http://localhost:8080/api/products/x */
 
-productsRouter.post("/api/products", (req, res) => {
+productsRouter.post("/api/products", async (req, res) => {
     try {
         const { title, description, code, price, stock, category } = req.body;
-
-        console.log(req.body)
 
         if (!title || !description || !code || !price || !stock || !category) {
             res.send("Todos los campos son obligatorios")
@@ -51,8 +52,13 @@ productsRouter.post("/api/products", (req, res) => {
             stock,
             category
         }
+
         products.push(newObj)
+
+        await fs.writeFile("products.txt", JSON.stringify(products));
+
         res.send({ message: "producto agregado satisfactoriamente" })
+
     } catch (error) {
         res.send({ message: error })
     }
@@ -72,13 +78,12 @@ http://localhost:8080/api/products
 }
 */
 
-productsRouter.put("/api/products/:pid", (req, res) => {
+productsRouter.put("/api/products/:pid", async (req, res) => {
     try {
         const { pid } = req.params;
         const { title, description, code, price, statusProduct, stock, category } = req.body;
 
-        console.log(req.params)
-        console.log(req.body)
+        const data = await fs.readFile("products.txt", "utf-8");
 
         if (!title || !description || !code || !price || !statusProduct || !stock || !category) {
             res.send("Todos los campos son obligatorios")
@@ -105,7 +110,7 @@ productsRouter.put("/api/products/:pid", (req, res) => {
             category
         }
 
-        const isExist = products.find(ele => ele.id === parseInt(pid));
+        const isExist = JSON.parse(data).find(ele => ele.id === parseInt(pid));
 
         if (!isExist) {
             res.send("El id solicitado no existe en la base de datos")
@@ -114,9 +119,10 @@ productsRouter.put("/api/products/:pid", (req, res) => {
 
         const newArr = products.map(ele =>
             ele.id === parseInt(pid) ?
-                { ...UpdObj } : ele);
+                { id: ele.id, ...UpdObj } : ele);
 
-        console.log(newArr);
+        await fs.writeFile("products.txt", JSON.stringify(newArr));
+
         res.send({ message: "producto modificado" })
     } catch (error) {
         res.send({ message: error })
@@ -131,27 +137,30 @@ http://localhost:8080/api/products/x
     "title": "Product Title",
     "description": "Product Description",
     "code": "Product Code",
-    "price": 1000,
-    "statusProduct": false,
+    "price": 999,
+    "statusProduct": true,
     "stock": 100,
     "category": "Product Category"
 }
 */
 
-productsRouter.delete("/api/products/:pid", (req, res) => {
+productsRouter.delete("/api/products/:pid", async (req, res) => {
     try {
         const { pid } = req.params;
 
-        const index = products.findIndex(ele =>
+        const data = await fs.readFile("products.txt", "utf-8");
+
+        const index = JSON.parse(data).findIndex(ele =>
             ele.id === parseInt(pid));
 
-            console.log(index)
         if (index < 0) {
             res.send({ message: "ID del producto no existe" })
             return
         }
 
         products.splice(index, 1);
+
+        await fs.writeFile('products.txt', JSON.stringify(products))
 
         res.send({ message: "producto eliminado" })
     } catch (error) {
