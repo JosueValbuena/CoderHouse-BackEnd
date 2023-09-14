5952b9fda6cb8dd7d41448f9b7f805a6cd287b9a
Josue <joshvlbn@gmail.com>
2023-09-14 00:49:42 -0300

    consultas get api cart, api cart id y post api cart funcionando.  consulta post api cart cid product pid con problemas

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 const prodctsRouter = require('./routes/products');
+const cartRouter = require('./routes/carts');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', prodctsRouter)
+app.use('/', cartRouter);
 
 app.get('/', (req, res) => {
     res.send('Hola desde Express')
