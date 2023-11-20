cf6b24afbd8147c79d683ae8a97854db3a9bf19a
Josue <joshvlbn@gmail.com>
2023-11-19 22:20:41 -0300

    agregado manejo de errores en entorno mongo, middleware para manejo de rutas erroneas y configuracion de coors

--- src/app.js
+++ src/app.js
@@ -7,14 +7,14 @@ import passport from "passport";
 
 dotenv.config();
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: [process.env.ORIGIN_ONLINE, process.env.ORIGIN_OFFLINE],
     methods: 'GET, POST, PUT, DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true
@@ -22,13 +22,20 @@ app.use(cors({
 app.use(passport.initialize());
 
 const mongoEnviroment = async () => {
-    await mongoose.connect(process.env.DB_SECRET_KEY);
-    console.log('Conectado con mongoose')
-}
+    try {
+        await mongoose.connect(process.env.DB_SECRET_KEY);
+        console.log('Conectado con mongoose');
+    } catch (error) {
+        console.error('Error al conectar con mogoose: ', error.mesage);
+    }
+};
 
 mongoEnviroment();
 
 app.use('/', router);
+app.use((req, res, next) => {
+    res.status(404).json({ error: 'Not Found' });
+});
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
